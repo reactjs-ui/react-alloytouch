5a520b5d7c5c11648637f09ae6194b9967056514
wangyanjun <linder0209@126.com>
2017-05-26 17:58:44 +0800

    init branch gh-pages

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# React AlloyTouch 更新日志
-
-## version 0.1.0  开启 React AlloyTouch 之旅
-
-
--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import ReactAlloyTouch from './ReactAlloyTouch';
-import reactPull from './ReactPull';
-import reactCarousel from './ReactCarousel';
-
-export const ReactPull = reactPull;
-export const ReactCarousel = reactCarousel;
-
-export default ReactAlloyTouch;
